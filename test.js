9f8ee8460a82ea466e65d9e1701d9474657c6723
clarkjeria <cjeria@cmu.edu>
2016-09-20 07:44:17 -0700

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-var supertest = require('supertest'),
-app = require('server');
-
-exports.car_should_return_cars = function(done){
-  supertest(app)
-  .get('/api/car')
-  .expect(200)
-  .end(done);
-};
